da1b92ab12a03d0adfdd4af790742da1523abff2
garv pathak <garvpathak34@gmail.com>
2026-01-20 15:53:50 +0530

    about api

--- src/main/java/com/example/day3/controller/StudentController.java
+++ src/main/java/com/example/day3/controller/StudentController.java
@@ -1,13 +1,18 @@
 package com.example.day3.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.day3.model.StudentModel;
+import com.example.day3.service.StudentService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StudentController {
-    @GetMapping("/")
-    public String student(){
-        return "Homepage for Student";
+    private  final StudentService service;
+    public StudentController(StudentService service){
+        this.service=service;
+    }
+    // create function api
+    @PostMapping("/add-student")
+    public StudentModel addStudent(@RequestBody StudentModel student){
+        return service.addStudent(student);
     }
 }
